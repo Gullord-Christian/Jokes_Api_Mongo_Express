acf1fbee12e8964ee2cad62bde819f28a9b71734
Christian Gullord <gullord.christian@gmail.com>
2022-07-16 02:38:09 -0500

    Jokes_Api

--- jokes_api/server/config/mongoose.config.js
+++ jokes_api/server/config/mongoose.config.js
@@ -9,3 +9,5 @@ mongoose.connect('mongodb://localhost/jokes_db', {
 })
     .then(() => console.log('Established a connection to the database'))
     .catch(err => console.log('Something went wrong when connecting to the database ', err));
+
+    // another note
